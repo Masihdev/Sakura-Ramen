01533afd0811ce08a8eaea11b83598aa9bdeb295
Masihdev <masihullahmassudi85@gmail.com>
2022-10-09 15:59:41 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+function translatePage() {
+    document.getElementById('title').innerHTML = 'SAKURA RAMEN';
+    document.getElementById('subtitle').innerHTML = 'DAS BESTE RAMEN IN DER STADT';
+    document.getElementById('menu').innerHTML = 'MENÜ';
+    document.getElementById('about').innerHTML = 'ÜBER UNS';
+
+    document.getElementById('location').innerHTML = 'STANDORTE';
+    document.getElementById('bestell').innerHTML = 'WIE MAN BESTELLT';
+    document.getElementById('pick1').innerHTML = 'Wähle<br> deine Nudel';
+    document.getElementById('pick2').innerHTML = 'Wähle <br>deine Brühe';
+    document.getElementById('belag').innerHTML = 'Füge <br>deine Beläge';
+    document.getElementById('menulist').innerHTML = 'UNSER MENÜ';
+    document.getElementById('chick').innerHTML = 'HÄNCHEN';
+    document.getElementById('beef').innerHTML = 'RIND';
+    document.getElementById('schrimp').innerHTML = 'SCHRIMP';
+    document.getElementById('chick1').innerHTML = 'HÄNCHEN';
+    document.getElementById('beef1').innerHTML = 'RIND';
+    document.getElementById('schrimp1').innerHTML = 'SCHRIMP';
+    document.getElementById('chick2').innerHTML = 'HÄNCHEN';
+    document.getElementById('beef2').innerHTML = 'RIND';
+    document.getElementById('schrimp2').innerHTML = 'SCHRIMP';
+    document.getElementById('ourLocation').innerHTML = 'FINDE UNS';
+    document.getElementById('tonkasu').innerHTML = 'Tonkasu Ramen';
+    document.getElementById('shio').innerHTML = 'Shio Ramen';
+    document.getElementById('miso').innerHTML = 'Spicy Miso Ramen';
+}
+
+function translatePagepersian() {
+    document.getElementById('title').innerHTML = 'ساکورا رامن  ';
+    document.getElementById('subtitle').innerHTML = 'بهترین رامن در شهر';
+    document.getElementById('menu').innerHTML = 'منو';
+    document.getElementById('about').innerHTML = 'در باره ما';
+
+    document.getElementById('location').innerHTML = 'مکان ها';
+    document.getElementById('bestell').innerHTML = 'نحوه سفارش';
+    document.getElementById('pick1').innerHTML = 'انتخاب<br>    مکرونی شما';
+    document.getElementById('pick2').innerHTML = ' انتخاب <br>آبگوشت شما';
+    document.getElementById('belag').innerHTML = 'اضافه<br> تاپنگ ها';
+    document.getElementById('menulist').innerHTML = 'منوی ما';
+    document.getElementById('tonkasu').innerHTML = 'تونکاسو رامن';
+    document.getElementById('chick').innerHTML = 'مرغ';
+    document.getElementById('beef').innerHTML = 'گوشت گاو';
+    document.getElementById('schrimp').innerHTML = 'شریمپ';
+    document.getElementById('miso').innerHTML = 'میسو رامن تند';
+    document.getElementById('chick1').innerHTML = 'مرغ';
+    document.getElementById('beef1').innerHTML = 'گوشت گاو';
+    document.getElementById('schrimp1').innerHTML = 'شریمپ';
+    document.getElementById('shio').innerHTML = 'شیو رامن';
+    document.getElementById('chick2').innerHTML = 'مرغ';
+    document.getElementById('beef2').innerHTML = 'گوشت گاو';
+    document.getElementById('schrimp2').innerHTML = 'شریمپ';
+    document.getElementById('ourLocation').innerHTML = 'ما را پیدا کنید در';
+}
